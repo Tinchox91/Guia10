b693fc00a2da5673386495a80670bc9d444ff5af
Tincho <tinchox1991@gmail.com>
2023-05-03 18:48:05 -0300

    primeros ejercicios

---
+++ Colecciones-Eejercicio_1_y_2/src/Inicio/Inicio.java
@@ -0,0 +1,17 @@
+
+package Inicio;
+
+import Arraylist.Lista_RazaPerro;
+
+
+public class Inicio {
+
+    
+    public static void main(String[] args) {
+        Lista_RazaPerro dog = new Lista_RazaPerro();
+        dog.agregarRaza();
+        
+       
+    }
+    
+}
